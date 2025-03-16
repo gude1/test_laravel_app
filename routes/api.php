cc5ee2abbe1a0dd23e2debd9b863c83b4f0885a9
Gideon Owolabi <owolabigidslab@gmail.com>
2025-03-16 10:10:38 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get("/hi", function () {
+    return response()->json(["message" => "hello"]);
+});
