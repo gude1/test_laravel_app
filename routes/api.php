20a3dffc7c584644d056298bcaaa76965b0a822e
Gideon Owolabi <owolabigidslab@gmail.com>
2025-03-16 15:30:04 +0100

    additional test

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,21 @@ Route::get('/user', function (Request $request) {
 Route::get("/hi", function () {
     return response()->json(["message" => "hello"]);
 });
+
+
+Route::get("/run-artisan", function () {
+    try {
+        Artisan::call('migrate', [
+            '--force' => true,
+        ]);
+        // Artisan::call("storage:link");
+        // Artisan::call("l5-swagger:generate");
+        return response()->json([
+            "message" => "Artisan commands ran succcessfully"
+        ], 200);
+    } catch (\Throwable $th) {
+        return response()->json([
+            "error" => "Failed to run artisan commands " . $th->getMessage()
+        ], 500);
+    }
+});
